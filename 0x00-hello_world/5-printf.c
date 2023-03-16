ce601547fb638f0e8376cfef2a2f4d2a61474d1e
Bckforgood <t.abdo.khbaza@gmail.com>
2023-03-16 07:07:32 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
 int main(void)
 {
-    printf("%s, %s\n", "With proper grammar", "but the outcome is a piece of art");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

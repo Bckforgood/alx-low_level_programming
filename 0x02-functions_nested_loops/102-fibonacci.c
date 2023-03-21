27faaaeda9c24bb001cb439caca5c01f50a3afa1
Bckforgood <t.abdo.khbaza@gmail.com>
2023-03-21 09:45:31 -0700

    ss

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
+#include <main.h>
 
-	return (0);
+void fibonacci_sequence(int n, int sequence[]) {
+    /* Computes the first n numbers of the Fibonacci sequence */
+    sequence[0] = 1;
+    sequence[1] = 2;
+    for (int i = 2; i < n; i++) {
+        sequence[i] = sequence[i-1] + sequence[i-2];
+    }
+}
+
+int main() {
+    int n = 50;
+    int fib_sequence[n];
+
+    fibonacci_sequence(n, fib_sequence);
+
+    // Print the first 50 Fibonacci numbers separated by commas
+    for (int i = 0; i < n-1; i++) {
+        printf("%d, ", fib_sequence[i]);
+    }
+    printf("%d\n", fib_sequence[n-1]);
+
+    return 0;
 }

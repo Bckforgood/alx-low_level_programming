94c443a34e7eb1ac5af207764b6c379a6438ffa6
Bckforgood <t.abdo.khbaza@gmail.com>
2023-03-21 17:15:38 -0700

    sh

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
-#include "main.h"
-/**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
- */
-void print_times_table(int n)
-{
-
-	return (0);
+#include <stdio.h>
+#include <main.h>
+int main() {
+    int n;
+    printf("Enter n (0 <= n <= 15): ");
+    scanf("%d", &n);
+    printf("Times table for %d:\n", n);
+    print_times_table(n);
+    return 0;
 }
+

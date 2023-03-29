5bdf8894460df408c7b99528a1a167e9481cfbdb
Bckforgood <t.abdo.khbaza@gmail.com>
2023-03-29 16:53:49 -0700

    test push

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,15 @@
 #include "main.h"
+#include <string.h>
+
+/**
+*_strcat - concatenates two strings
+**@dest: pointer to char
+**@src: pointer to char
+*Return: concantinated pointer string
+*/
+
 char *_strcat(char *dest, char *src)
 {
-int i, j;
-
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
+	strcat(dest, src);
+	return (dest);
 }

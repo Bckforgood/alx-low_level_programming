2024fe7b89c32b3e5ca8345c960a535c2feace78
Bckforgood <t.abdo.khbaza@gmail.com>
2023-04-13 16:26:54 -0700

    more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,25 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-    if (s1 == NULL) s1 = "";
-    if (s2 == NULL) s2 = "";
-    
-    size_t s1_len = strlen(s1);
-    size_t s2_len = strlen(s2);
-    
-    if (n >= s2_len) {
+/**
+ * string_nconcat - Concatenates two strings
+ * @s1: First string to concatenate
+ * @s2: Second string to concatenate
+ * @n: Maximum number of bytes to use from s2
+ *
+ * Return: Pointer to the newly allocated string, or NULL if allocation fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    char *concatenated_str;
+    unsigned int s1_len = 0, s2_len = 0;
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    s1_len = strlen(s1);
+    s2_len = strlen(s2);
+
+    if (n >= s2_len)
         n = s2_len;
-    }
-    
-    char *result = malloc(s1_len + n + 1); // +1 for null terminator
-    if (result == NULL) {
-        return NULL;
-    }
-    
-    memcpy(result, s1, s1_len);
-    memcpy(result + s1_len, s2, n);
-    result[s1_len + n] = '\0';
-    
-    return result;
+
+    concatenated_str = malloc(sizeof(char) * (s1_len + n + 1));
+
+    if (concatenated_str == NULL)
+        return (NULL);
+
+    memcpy(concatenated_str, s1, s1_len);
+    memcpy(concatenated_str + s1_len, s2, n);
+    concatenated_str[s1_len + n] = '\0';
+
+    return (concatenated_str);
 }
+

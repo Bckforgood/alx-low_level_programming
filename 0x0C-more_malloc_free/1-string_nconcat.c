b8c23d752f23e8ce746d8fcc065c7521a00f3bb1
Bckforgood <t.abdo.khbaza@gmail.com>
2023-04-13 16:50:01 -0700

    more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,48 @@
 #include <main.h>
 #include <stdlib.h>
-#include <string.h>
-
-/**
- * string_nconcat - Concatenates two strings
- * @s1: First string to concatenate
- * @s2: Second string to concatenate
- * @n: Maximum number of bytes to use from s2
+#include <stdio.h>
+/*
+ * string_nconcat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * @n: mem size of concatnated string
  *
- * Return: Pointer to the newly allocated string, or NULL if allocation fails
+ * Return: NULL if function fails
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *concatenated_str;
-    unsigned int s1_len = 0, s2_len = 0;
+	char *ptr;
+	unsigned int x, y, z;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+	{
+		x = 0;
+	}
+	else
+	{
+		for (x = 0; s1[x]; ++x)
+			;
+	}
+	if (s2 == NULL)
+	{
+		y = 0;
+	}
+	else
+	{
+		for (y = 0; s2[y]; ++y)
+			;
+	}
+	if (y > n)
+		y = n;
+	ptr = malloc(sizeof(char) * (x + y + 1));
 
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
-
-    if (n >= s2_len)
-        n = s2_len;
-
-    concatenated_str = malloc(sizeof(char) * (s1_len + n + 1));
-
-    if (concatenated_str == NULL)
-        return (NULL);
-
-    memcpy(concatenated_str, s1, s1_len);
-    memcpy(concatenated_str + s1_len, s2, n);
-    concatenated_str[s1_len + n] = '\0';
-
-    return (concatenated_str);
+	if (ptr == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+		ptr[z] = s1[z];
+	for (z = 0; z < y; z++)
+		ptr[z + x] = s2[z];
+	ptr[x + y] = '\0';
+	return (ptr);
 }
-

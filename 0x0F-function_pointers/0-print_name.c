703517a31e9241b63697672ab115eca497f38080
Bckforgood <t.abdo.khbaza@gmail.com>
2023-04-19 18:44:38 -0700

    0x0f

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -13,30 +13,13 @@ void print_name(char *name, void (*f)(char *))
 {
     f(name);
 }
-
-/**
- * print - prints a name
- *
- * @name: name to print
- *
- * Return: void
- */
-
 void print(char *name)
 {
     printf("Name: %s\n", name);
 }
-
-/**
- * main - calls print_name function to print a name
- *
- * Return: 0
- */
-
 int main(void)
 {
     char *name = "John Doe";
     print_name(name, print);
     return (0);
 }
-

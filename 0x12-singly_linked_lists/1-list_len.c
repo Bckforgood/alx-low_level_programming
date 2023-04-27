8e25547ef92057bc3b0ea157a8bd065afbfb9c2f
Bckforgood <t.abdo.khbaza@gmail.com>
2023-04-27 04:54:47 +0100

    0x012

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements in the list
+ */
+size_t list_len(const list_t *h)
+{
+    const list_t *current;
+    size_t count = 0;
+
+    for (current = h; current != NULL; current = current->next)
+    {
+        count++;
+    }
+
+    return (count);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list to be freed.
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}

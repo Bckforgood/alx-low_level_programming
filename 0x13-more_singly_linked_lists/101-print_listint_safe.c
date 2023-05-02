46b22efbb851195e1f5f1daf5f957e562e6ba55a
Bckforgood <t.abdo.khbaza@gmail.com>
2023-05-01 19:30:36 -0700

    fix101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,42 +1,35 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: A pointer to the head node of the linked list.
+ * print_listint_safe - Prints a listint_t linked list
+ * @head: Pointer to the beginning of the list
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *curr = head, *prev = NULL;
 	size_t count = 0;
-	const listint_t *slow = head, *fast = head;
+	int first_repeat_found = 0;
 
-	while (slow && fast && fast->next)
+	while (curr)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
+		printf("[%p] %d\n", (void *)curr, curr->n);
 		count++;
-		slow = slow->next;
-		fast = fast->next->next;
 
-		if (slow == fast)
+		if (curr < prev && !first_repeat_found)
 		{
-			printf("[%p] %d\n", (void *)slow, slow->n);
-			count++;
-			printf("-> [%p] %d\n", (void *)fast->next, fast->next->n);
-			count++;
-			break;
+			printf("-> [%p] %d\n", (void *)curr, curr->n);
+			first_repeat_found = 1;
 		}
+
+		prev = curr;
+		curr = curr->next;
 	}
 
-	if (!slow || !fast || !fast->next)
-	{
-		while (head)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			count++;
-			head = head->next;
-		}
-	}
+	if (!first_repeat_found)
+		printf("-> [NULL]\n");
 
 	return (count);
 }
+

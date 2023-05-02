94120ee53981ef500daf66ed75d83896c8a63dd2
Bckforgood <t.abdo.khbaza@gmail.com>
2023-05-01 20:06:37 -0700

    fix102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,26 +10,38 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t count = 0;
-	listint_t *current, *next;
+    size_t count = 0;
+    listint_t *current, *next;
 
-	if (!h || !*h)
-	return (0);
+    if (!h || !*h)
+        return (0);
 
-	current = *h;
-	while (current)
-	{
-		next = current->next;
-		free(current);
-		count++;
-		if (next >= current)
-		{
-			printf("-> [%p] %d\n", (void *)next, next->n);
-			break;
-		}
-		current = next;
-	}
-	*h = NULL;
+    current = *h;
+    while (current)
+    {
+        /* Keep a pointer to the next node before freeing the current node */
+        next = current->next;
+        
+        /* Free the current node and increment the counter */
+        free(current);
+        count++;
+        
+        /* Check if the address of the next node is lower than or equal to the */
+        /* address of the current node. If true, it means that we have a cycle */ 
+        /* in the list, so we break the loop to avoid an infinite loop */
+        if (next <= current)
+        {
+            printf("-> [%p] %d\n", (void *)next, next->n);
+            break;
+        }
+        
+        /* Move to the next node */
+        current = next;
+    }
+    
+    /* Set the head pointer to NULL*/
+    *h = NULL;
 
-	return (count);
+    return (count);
 }
+

497ac57a1de4a27cf00448d8db19428ee9cea04a
Bckforgood <t.abdo.khbaza@gmail.com>
2023-05-02 17:23:13 -0700

    fix101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,44 +3,36 @@
 #include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely
- * @head: Pointer to the head node of the list
+ * print_listint_safe - Prints a listint_t linked list, handles loops
+ *                     and prints a message if a loop is found.
  *
- * Return: Number of nodes in the list
+ * @head: A pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *curr = head;
-	const listint_t *printed[100];
-	size_t printed_count = 0;
-	int loop_detected = 0;
+	long int diff;
 
-	while (curr)
+	if (head == NULL)
+		exit(98);
+
+	while (head != NULL)
 	{
-		size_t i;
+		diff = head - head->next;
+		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
 
-		/* Check if this node has already been printed */
-		for (i = 0; i < printed_count; i++)
+		if (diff <= 0)
 		{
-			if (curr == printed[i])
-			{
-				printf("-> [%p] %d\n", (void *)curr, curr->n);
-				loop_detected = 1;
-				break;
-			}
-		}
-
-		if (loop_detected)
-		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
-
-		printf("[%p] %d\n", (void *)curr, curr->n);
-		printed[printed_count++] = curr;
-		count++;
-
-		curr = curr->next;
+		else
+		{
+			head = head->next;
+		}
 	}
 
 	return (count);

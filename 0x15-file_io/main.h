744fb9ad65e72634d4f0d5b186d88879085e2b7e
Bckforgood <t.abdo.khbaza@gmail.com>
2023-05-08 16:28:09 -0700

    m

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,8 @@
 #include <stdlib.h>
 #include <errno.h>
 int main(int argc, char *argv[]);
+int fd_from, fd_to, rd, wr;
+char buffer[BUF_SIZE];
 void exit_with_error(char *message, char *arg, int code);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

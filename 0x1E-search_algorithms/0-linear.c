83644ea304844c1739f92adfa3f24187541bfb00
Bckforgood <t.abdo.khbaza@gmail.com>
2023-10-11 12:06:08 -0700

    fix errors

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,7 +13,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	if (array == NULL)
 		return (-1);
-	for (size_t i = 0; i < size; i++)
+	size_t i; /* Declaration moved to the top */
+	while (i < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
